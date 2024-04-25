0888887e1f485a0ab3921cff210e3e85b4526a93
Carlos Mbendera <carlosmbendera@gmail.com>
2024-04-25 18:30:49 -0500

    Initial Commit

---
+++ MyMacLLAMA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyMacLLAMA
+//
+//  Created by Carlos Mbendera on 25/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyMacLLAMA/MyMacLLAMAApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMacLLAMAApp.swift
+//  MyMacLLAMA
+//
+//  Created by Carlos Mbendera on 25/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyMacLLAMAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
